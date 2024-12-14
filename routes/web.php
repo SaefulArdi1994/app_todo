87668e70b6bd74596c56033f12bf05bf4de6b71f
Saeful Ardi <saeful.4rdi@gmail.com>
2024-12-15 05:57:56 +0700

    membuat middleware

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,10 @@ Route::get('/user/login', [UserController::class, 'login'])->name('login');
 Route::post('/user/login', [UserController::class, 'doLogin'])->name('login.post');
 Route::get('/user/logout', [UserController::class, 'logout'])->name('logout');
 
-Route::get('/todo', [TodoController::class, 'index'])->name('todo');
-Route::post('/todo', [TodoController::class, 'store'])->name('todo.add');
-Route::put('/todo/{id}', [TodoController::class, 'update'])->name('todo.update');
-Route::delete('/todo/{id}', [TodoController::class, 'destroy'])->name('todo.delete');
+Route::middleware('auth')->group(function() {
+    Route::get('/todo', [TodoController::class, 'index'])->name('todo');
+    Route::post('/todo', [TodoController::class, 'store'])->name('todo.add');
+    Route::put('/todo/{id}', [TodoController::class, 'update'])->name('todo.update');
+    Route::delete('/todo/{id}', [TodoController::class, 'destroy'])->name('todo.delete');
+});
 
